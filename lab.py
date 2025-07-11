23162a8c93156b757127df2f0100b3160e258f2a
KevinMendapara <kevinsadguru@gmail.com>
2025-07-11 18:41:26 +0530

    Create lab.py
    simple python program

---
+++ lab.py
@@ -0,0 +1,7 @@
+num1=25
+num2=50
+sum=num1+num2
+print("The sum of", num1, "and", num2, "is:", sum)
+print("This is a simple addition program.") 
+# End of the lab.py file
+# You can run this code to see the output of the addition operation.
