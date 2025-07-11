dbb8026ed9d377b39f6fbeaf253d45e690390698
ManavAIML <vaikunth2213@gmail.com>
2025-07-11 19:19:50 +0530

    Delete lab.py

--- lab.py
+++
@@ -1,7 +0,0 @@
-num1=25
-num2=50
-sum=num1+num2
-print("The sum of", num1, "and", num2, "is:", sum)
-print("This is a simple addition program.") 
-# End of the lab.py file
-# You can run this code to see the output of the addition operation.
